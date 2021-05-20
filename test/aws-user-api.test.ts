36ae46e7c01f152f28af7a3a989db92e5c346d43
Ioannis Giakoumidis <ioannis.giakoumidis@york.ac.uk>
2021-05-20 18:11:26 +0100

    Initial commit

---
+++ test/aws-user-api.test.ts
@@ -0,0 +1,13 @@
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as AwsUserApi from '../lib/aws-user-api-stack';
+
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new AwsUserApi.AwsUserApiStack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(matchTemplate({
+      "Resources": {}
+    }, MatchStyle.EXACT))
+});
